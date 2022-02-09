4170fa73b1ec58c4d13f1ababd06307e37757c27
probe_1 <alexeyshu86@mail.ru>
2022-02-10 02:03:39 +0700

    task 21 - complete

--- task_21/main.c
+++ task_21/main.c
@@ -2,5 +2,28 @@
 
 void main()
 {
+	unsigned int hours;
+	float rate, salary;
 
+	printf("Enter the amount of hours worked (-1, if the input is finished):");
+	scanf_s("%u", &hours);
+
+	while (hours != -1)
+	{
+		printf("Enter the employee's hourly rate: ");
+		scanf_s("%f", &rate);
+
+		if (hours <= 40)
+			salary = hours * rate;
+
+		if (hours > 40)
+			salary = 40 * rate + (/*(float)*/(hours - 40)) * rate * 1.5;
+
+		printf("Salary is $%.2f\n", salary);
+
+		printf("\nEnter the amount of hours worked (-1, if the input is finished):");
+		scanf_s("%u", &hours);
+	}
+
+	return 0;
 }
