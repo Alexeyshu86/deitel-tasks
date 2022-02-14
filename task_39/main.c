98aa34aed851cbd53c7f2d66371345d577fa0bbc
probe_1 <alexeyshu86@mail.ru>
2022-02-14 22:02:29 +0700

    fixed task_39

--- task_39/main.c
+++ task_39/main.c
@@ -5,23 +5,26 @@ int main()
 	int num, x, y, count;
 	
 	count = 0;
-	num = 77777777;
+	num = 755778;
 	x = 10;
 	y = 1;
 
 	if (num == 7)
 		count++;
-
-	while (num / x != 0)
+	else
 	{
-		if (num % x / y == 7)
+		while (num / x != 0)
+		{
+			if (num % x / y == 7)
+				count++;
+			x *= 10;
+			y *= 10;
+		}
+
+		if (num / y == 7)
 			count++;
-		x *= 10;
-		y *= 10;
 	}
 
-	if (num / y == 7)
-		count++;
 
 	printf("%d", count);
 
